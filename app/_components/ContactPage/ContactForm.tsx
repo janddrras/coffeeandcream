15ed8b95985772d94450de10df17e404232ec622
janddrras <janddrras@gmail.com>
2023-07-25 13:51:35 +0300

    button complete

--- app/_components/ContactPage/ContactForm.tsx
+++ app/_components/ContactPage/ContactForm.tsx
@@ -5,6 +5,7 @@ import TextInput from "@/app/_components/ui/TextInput"
 import AreaInput from "@/app/_components/ui/AreaInput"
 import { useState } from "react"
 import Checkbox from "@/app/_components/ui/Checkbox"
+import Button from "../ui/Button"
 
 const ContactForm = () => {
   const [name, setName] = useState("")
@@ -12,17 +13,27 @@ const ContactForm = () => {
   const [message, setMessage] = useState("")
   const [privacyAgreement, setPrivacyAgreement] = useState(false)
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log({ name, email, message, privacyAgreement })
+  }
+
   return (
     <section className="bg-cream-30/10 dark:bg-coffee-90/200">
       <div className="pt-16 md:pb-32 pb-8 px-2 md:px-8">
         <SectionTitle>Send me a message</SectionTitle>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <TextInput label="Name or Company" onChange={setName} value={name} type="text" isRequired />
           <TextInput label="Email address" onChange={setEmail} value={email} type="email" isRequired />
           <AreaInput label="Your Message" onChange={setMessage} value={message} type="textarea" isRequired />
-          <Checkbox isSelected={privacyAgreement} onChange={setPrivacyAgreement} labelStyles="text-sm">
-            I agree with the privacy statement and to be contacted by Coffeeandcream office
-          </Checkbox>
+          <div className="flex items-center justify-between gap-8">
+            <Checkbox isSelected={privacyAgreement} onChange={setPrivacyAgreement} labelStyles="text-sm">
+              I agree with the privacy statement and to be contacted by Coffeeandcream office
+            </Checkbox>
+            <Button type="submit" isDisabled={!privacyAgreement}>
+              SEND
+            </Button>
+          </div>
         </form>
       </div>
     </section>
---
+++ app/_lib/variants/buttonVariants.ts
@@ -0,0 +1,27 @@
+import { Variants } from "framer-motion"
+
+export const line1Variants: Variants = {
+  initial: { x: -150 },
+  hover: { x: 0, transition: { duration: 0.2, delay: 0.2 } },
+  pressed: { x: 150, transition: { duration: 0.2, delay: 0.2 } }
+}
+export const line2Variants: Variants = {
+  initial: { x: 150 },
+  hover: { x: 0, transition: { duration: 0.2, delay: 0.3 } },
+  pressed: { x: 150, transition: { duration: 0.2, delay: 0.2 } }
+}
+export const backgroundVariants: Variants = {
+  initial: { x: 0 },
+  hover: { x: -150, transition: { duration: 0.2, delay: 0.1 } },
+  pressed: { x: 0, transition: { duration: 0.2, delay: 0.3 } }
+}
+export const textVariants: Variants = {
+  initial: { x: 0 },
+  hover: { x: 15, transition: { duration: 0.2, delay: 0.3 } },
+  pressed: { x: 100, transition: { duration: 0.2, delay: 0.2 } }
+}
+export const iconVariants: Variants = {
+  initial: { x: -50 },
+  hover: { x: 0, transition: { duration: 0.2, delay: 0.3 } },
+  pressed: { x: 50, transition: { duration: 0.2, delay: 0.2 } }
+}
